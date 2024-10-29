d3e93ebc4b36dd4a5e980a15888cc9177b0d4046
mohammed ali <mohammedalahmadi@my.whccd.edu>
2024-10-28 19:14:07 -0700

    finished the pattern task

--- script3.js
+++ script3.js
@@ -9,3 +9,11 @@
 //  # # # #
 // # # # #
 //  # # # #
+let size = 8;
+for (let row = 0; row < size; row++) {
+    let line = "";
+    for (let col = 0; col < size; col++) {
+        line += (row + col) % 2 === 0 ? "#" : " ";
+    }
+    console.log(line);
+}
